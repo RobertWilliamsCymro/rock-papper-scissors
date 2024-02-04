c4e85dd94e8493206c08d29a1a7cd7987f8e3c51
RobertWilliamsCymro <robert.williams@jisc.ac.uk>
2024-02-04 15:59:54 +0000

    setting up the project, adding in first stabs at layout.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <style>
+          @import url('https://fonts.googleapis.com/css2?family=Rubik:wght@500;600;700&display=swap');
+        </style>
+        <link rel="icon" href="/favicon-32x32.png" />
+      </Head>
       <body>
         <Main />
         <NextScript />
