ace39f4a589698fe82a8ce211bc69a9db0ae02db
RobertWilliamsCymro <robert.williams@jisc.ac.uk>
2024-03-18 19:01:27 +0000

    Basic logic for the game

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,78 @@
+import { useState } from "react";
+
 export default function Home() {
+
+  const [score, setScore] = useState(0);
+  // this is a function that will be called when the button is clicked
+  // it will: take the value I select, then select a random value, either "rock", "scissors" or "paper"
+  // if I select "rock" and the random value is "scissors" I win a point, if the random value is "paper" I lose a point
+
+  function CalculateScore(selectedValue: string) {
+    // variable to
+    const randomValues = ["rock", "paper", "scissors"];
+    const randomValue = randomValues[Math.floor(Math.random() * randomValues.length)];
+
+    if (selectedValue === "rock" && randomValue === "rock") {
+      console.log("It's a tie");
+    }
+    if (selectedValue === "paper" && randomValue === "rock") {
+      console.log("You win!");
+      setScore(score + 1);
+      console.log(score);
+    }
+    if (randomValue === "paper" && selectedValue === "rock") {
+      console.log("You lose");
+      setScore(score - 1);
+    }
+
+    if (selectedValue === "paper" && randomValue === "paper") {
+      console.log("It's a tie");
+    }
+
+    if (selectedValue === "rock" && randomValue === "paper") {
+      console.log("You win!");
+      setScore(score + 1);
+      console.log(score);
+    }
+
+    if (randomValue === "scissors" && selectedValue === "paper") {
+      console.log("You lose");
+      setScore(score - 1);
+    }
+
+    if (selectedValue === "scissors" && randomValue === "paper") 
+    {
+      console.log("You win!");
+      setScore(score + 1);
+      console.log(score);
+    }
+
+    if (selectedValue === "rock" && randomValue === "scissors") {
+      console.log("You lose!");
+      setScore(score - 1);
+      console.log(score);
+    }
+    
+    if (randomValue === "scissors" && selectedValue === "scissors") {
+      console.log("It's a tie");
+
+    }
+
+  }
+
   return (
     <>
-
-      <main className="flex justify-center min-h-screen">        
-        <div className="border-slate-200">
-          <img className="mt-8" src="logo.svg" alt="A logo of the game Rock, Paper, Scissors" />
-          <div className="bg-white rounded-md flex flex-col justify-center items-center py-3">
-            <h2 className="">Score</h2>
-            <p className="">12</p>
-          </div>
-        </div>
-      </main>
+      {/* Write basic javascript code */}
+      <p className="text-white">{score}</p>
+      <button value="rock" className="text-white" onClick={() => CalculateScore("rock")}>
+        Rock
+      </button><br />
+      <button value="scissors" className="text-white" onClick={() => CalculateScore("scissors")}>
+        Scissors
+      </button><br />
+      <button value="paper" className="text-white" onClick={() => CalculateScore("paper")}>
+        Paper
+      </button>
     </>
   )
 }
-
